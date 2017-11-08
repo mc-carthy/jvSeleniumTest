d649a742e32bfe4f8e69f343daf6c5e9fb130ac7
Mick Mccarthy <Mick.Mccarthy@centre4.local>
2017-11-08 17:10:04 +0000

    Make use of before/after hooks

--- src/smokeTests/PageTitleJUnit.java
+++ src/smokeTests/PageTitleJUnit.java
@@ -1,20 +1,33 @@
 package smokeTests;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 
 public class PageTitleJUnit {
 	
+	WebDriver driver;
+	
 	String url = "http://sdettraining.com/trguitransactions/AccountManagement.aspx";
 	String expectedTitle = "SDET Training | Account Management";
+	
+	@Before
+	public void Setup() {
+		driver = utilities.DriverFactory.open("firefox");
+		driver.get(url);
+	}
+	
+	@After
+	public void TearDown() {
+		driver.close();
+	}
 
 	@Test
 	public void PageTitleTest() {
-		WebDriver driver = utilities.DriverFactory.open("firefox");
-		driver.get(url);
-		
-		
 		String actualTitle = driver.getTitle();
 		
+		Assert.assertEquals(expectedTitle, actualTitle);
 	}
 }
