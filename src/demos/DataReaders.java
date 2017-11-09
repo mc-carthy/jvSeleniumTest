3997aa728b1d0f1c8775b00bd141d9c669013531
Mick Mccarthy <Mick.Mccarthy@centre4.local>
2017-11-09 10:59:11 +0000

    Create Excel reader

--- src/demos/DataReaders.java
+++ src/demos/DataReaders.java
@@ -5,7 +5,8 @@ import java.util.List;
 public class DataReaders {
 
 	public static void main(String[] args) {
-		readCSV();
+//		readCSV();
+		readXLS();
 	}
 	
 	public static void readCSV() {
@@ -13,9 +14,22 @@ public class DataReaders {
 		List<String[]> records = utilities.CSV.get(filename);
 		
 		for (String[] record : records) {
+			System.out.println("\nNEW RECORD");
 			for (String field : record)
 			{
 				System.out.println(field);		
+			}
+		}
+	}
+	
+	public static void readXLS() {
+		String filename = "C:\\Users\\mick.mccarthy\\Documents\\testing\\seleniumJavaSetUp\\UserLogin.xls";
+		String[][] data = utilities.Excel.get(filename);
+		
+		for (String[] record : data) {
+			System.out.println("\nNEW RECORD");
+			for (String field : record) {
+				System.out.println(field);				
 			}
 		}
 	}
